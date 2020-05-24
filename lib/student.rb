2b4e690fbf98f90c034c0435b87821e93737ed9d
Rene Avila <reneavila1993@gmail.com>
2020-05-24 22:55:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,11 @@ class Student
   
   attr_accessor :name, :grade, :id
   
+<<<<<<< HEAD
   def initialize(name, grade, id = nil)
+=======
+  def initialize(id = nil, name, grade)
+>>>>>>> faa7dd25b5d0a7e35cad7b9109102773371d9e84
     @id = id
     @name = name
     @grade = grade
@@ -51,7 +55,16 @@ class Student
   end
   
   def self.new_from_db(row)
+<<<<<<< HEAD
     student = Student.new(row[1], row[2], row[0])
+=======
+    new_student = self.new  
+    binding.pry
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
+>>>>>>> faa7dd25b5d0a7e35cad7b9109102773371d9e84
   end
   
   def self.find_by_name(name)
